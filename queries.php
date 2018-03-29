d19d8a1860d02dd4adcae3fb1ee722dd7ddfe789
Y3ll0w <27010973+Y3ll0w@users.noreply.github.com>
2018-03-29 02:02:45 +0200

    Update queries.php

--- queries.php
+++ queries.php
@@ -1,51 +1,44 @@
 <?php
-error_reporting(0);
+//Server
 $ip = '0.0.0.0';
-$queryport = 27017;
+$queryport = 27015;
 
-//A2S_INFO
+//Socket
 $socket = @fsockopen("udp://".$ip, $queryport , $errno, $errstr, 1);
 stream_set_timeout($socket, 2);	
+
+//A2S_INFO
 fwrite($socket, "\xFF\xFF\xFF\xFF\x54Source Engine Query\x00");
 $response = fread($socket, 4096);
-fclose($socket);
 $packet = explode("\x00", substr($response, 6), 5);
 print "<pre>";
 print_r($packet);
 print "</pre>";
 
-//A2S_SERVERQUERY_GETCHALLENGE
-$socket2 = @fsockopen("udp://".$ip, $queryport , $errno, $errstr, 1);
-stream_set_timeout($socket2, 2);	
-fwrite($socket2, "\xFF\xFF\xFF\xFF\x56\x00\x00\x00\x00");
-$response2 = fread($socket2, 4096);
-fclose($socket2);
-$challenge = explode("\x41", substr($response2, 5));
+//A2S_SERVERQUERY_GETCHALLENGE	
+fwrite($socket, "\xFF\xFF\xFF\xFF\x56\x00\x00\x00\x00");
+$response = fread($socket, 4096);
+$challenge = explode("\x41", substr($response, 5));
 $challenge = $challenge[0];
 
 echo bin2hex($challenge);
 
 //A2S_RULES
-$socket3 = @fsockopen("udp://".$ip, $queryport , $errno, $errstr, 1);
-stream_set_timeout($socket3, 2);	
-fwrite($socket3, "\xFF\xFF\xFF\xFF\x56" . $challenge);
-$response3 = fread($socket3, 4096);
-fclose($socket3);
+fwrite($socket, "\xFF\xFF\xFF\xFF\x56" . $challenge);
+$response = fread($socket, 4096);
 
-$packet3 = explode("\x00", substr($response3, 6));
+$packet = explode("\x00", substr($response, 6));
 print "<pre>";
-print_r($packet3);
+print_r($packet);
 print "</pre>";
 
 //A2S_PLAYER
-$socket4 = @fsockopen("udp://".$ip, $queryport , $errno, $errstr, 1);
-stream_set_timeout($socket4, 2);	
-fwrite($socket4, "\xFF\xFF\xFF\xFF\x55" . $challenge);
-$response4 = fread($socket4, 4096);
-fclose($socket4);
+fwrite($socket, "\xFF\xFF\xFF\xFF\x55" . $challenge);
+$response = fread($socket, 4096);
+fclose($socket);
 
-$packet4 = explode("\x00", substr($response4, 6));
+$packet = explode("\x00", substr($response, 6));
 print "<pre>";
-print_r($packet4);
+print_r($packet);
 print "</pre>";
 ?>
